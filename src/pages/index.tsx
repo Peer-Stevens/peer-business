71cd7a128a3dc6c5aac3b96c261960dba38e9490
Andrew Jones <aej11@hotmail.com>
2021-12-27 16:26:37 -0500

    Cleaned up the UI, added hover states, added Zip field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,61 @@
 import { useState } from "react";
+import { SearchResult } from "../components/SearchResult";
 import { useSearchPlaces } from "../hooks/useSearchPlaces";
-import Link from "next/link";
 
 const Index = (): JSX.Element => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [zip, setZip] = useState("");
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { places, loading } = useSearchPlaces({
-    searchText: searchTerm,
+    searchText: searchTerm + " " + zip,
   });
 
   return (
     <>
-      <div className="pt-2 relative mx-auto text-gray-600">
+      <div className="pt-2 relative text-gray-600 max-w-md text-left">
         <form
           onSubmit={e => {
             e.preventDefault();
             /* eslint-disable */
-            // @ts-ignorea
+            // @ts-ignore
             setSearchTerm(e.target["search"].value);
+            // @ts-ignore
+            setZip(e.target["zip"].value);
             /* eslint-enable */
           }}
+          // className="grid grid-cols-2"
         >
-          <input
-            className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-            type="search"
-            name="search"
-            placeholder="Search"
-          />
-          <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-            <svg
-              className="text-gray-600 h-4 w-4 fill-current"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              version="1.1"
-              id="Capa_1"
-              x="0px"
-              y="0px"
-              viewBox="0 0 56.966 56.966"
-              xmlSpace="preserve"
-              width="512px"
-              height="512px"
-            >
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-            </svg>
+          <label htmlFor={"search"}>
+            Name:
+            <input
+              className="border-2 border-gray-600 rounded-lg max-w-full w-full bg-white h-10 px-5 mb-5 text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search"
+            />
+          </label>
+          <label htmlFor={"zip"}>
+            Zip Code:
+            <input
+              className="border-2 border-gray-600 bg-white max-w-full w-full h-10 px-5 mb-4 rounded-lg text-sm focus:outline-none"
+              type="zip"
+              name="zip"
+              placeholder="Zip Code"
+            />
+          </label>
+          <button
+            type="submit"
+            className="border-2 border-gray-600 p-2.5 rounded-md w-full hover:shadow-md transition-all text-black"
+          >
+            🔎 Search
           </button>
         </form>
       </div>
       {loading && <div>Loading...</div>}
       {places &&
         places.length > 0 &&
-        places.map(place => (
-          <Link href={`/place/${place.place_id!}`} key={place.place_id}>
-            <div className="border-2 border-black my-4 flex">
-              {place.geometry && (
-                <img
-                  src={`https://maps.googleapis.com/maps/api/staticmap?size=500x500&markers=color:black%7C${place.geometry.location.lat},${place.geometry.location.lng}&zoom=19&key=AIzaSyDr_urYOjCyJyTzMjYmeMBNChgZAUbPoqw`}
-                  alt={place.name}
-                  style={{ width: "300px" }}
-                />
-              )}
-              <div className="ml-4">
-                <p className="font-semibold">{place.name}</p>
-                <p>{place.formatted_address}</p>
-              </div>
-            </div>
-          </Link>
-        ))}
+        places.map(place => <SearchResult place={place} />)}
     </>
   );
 };
